dd74eab5a601ece398251e866f5e88b05b58ca2c
Teodor Birghisan <birghisanteodor@yahoo.com>
2021-11-26 15:31:41 +0200

    Moved LoginForm to widgets

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:vot_senat_client/widgets/login/login_input.dart';
+import 'package:vot_senat_client/widgets/login/login_form.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -11,64 +11,6 @@ class LoginPage extends StatelessWidget {
         title: const Text('Login'),
       ),
       body: const LoginForm(),
-    );
-  }
-}
-
-class LoginForm extends StatefulWidget {
-  const LoginForm({Key? key}) : super(key: key);
-
-  @override
-  LoginFormState createState() {
-    return LoginFormState();
-  }
-}
-
-class LoginFormState extends State<LoginForm> {
-  final _formKey = GlobalKey<FormState>();
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const LoginInputFied(
-            title: 'Username',
-            isPassword: false,
-          ),
-          const LoginInputFied(
-            title: 'Password',
-            isPassword: true,
-          ),
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  fixedSize: const Size(300, 50),
-                  primary: Colors.black87,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                ),
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text('Login in progress'),
-                      ),
-                    );
-                  }
-                },
-                child: const Text('LOGIN'),
-              ),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
